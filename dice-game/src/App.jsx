ce5beb5a8c4b7336e72858b860091a53316a63e9
vikash <vp3637637@gmail.com>
2024-02-27 19:58:24 +0530

    updated dice-game

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -2,9 +2,11 @@
 import { useState } from 'react'
 import Home from './components/Home'
 import Game from './components/Game'
+import song from './components/bgm.mp3'
 const App = () => {
-   const [play, setplay] = useState(false)
+   const [play, setplay] = useState(!false)
    const handlePlaybtn =(user)=>{
+      new Audio(song).play()
       setplay(user)
    }
   return (
--- dice-game/src/components/Game.jsx
+++ dice-game/src/components/Game.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './game.module.css'
+import sound from './dice.mp3'
+import wipe from './wipe.mp3'
+import win from './win.mp3'
 
 const Game = () => {
     let dices=[1,2,3,4,5,6]
     const [selected, setselected] = useState(0)
-    const [currentDice,setCurrentDice]=useState(0)
+    const [currentDice, setcurrentDice] = useState(0)
     let images=["images/dice_1.png","images/dice_2.png","images/dice_3.png","images/dice_4.png","images/dice_5.png","images/dice_6.png"]
     const [score, setscore] = useState(0)
+    const [rules, setrules] = useState(false)
 
     const generateRandom=()=>{
-        let random=Math.floor(Math.random()*6)
-        setCurrentDice(random)
-        // setselected(0)
-        console.log(selected+"selected");
-        console.log(random+"dice");
-        if(selected==currentDice){
-            setscore(score+(random+1))
-        }else{
-            setscore(score-(random+1))
-        }
+        if(selected>0){
+           new Audio(sound).play()
+           // setselected(0)
+           // console.log(selected+"selected");
+           // console.log(currentDice+"dice");
+           if(selected==currentDice+1){
+               new Audio(win).play()
+               setscore(score+1)
+            }else{
+                if(score>0){
+                    setscore(score-1)
+                }
+            }
+            setcurrentDice(Math.floor(Math.random()*6))
+       }
+    }
+    const resetscore=()=>{
+        new Audio(wipe).play()
+       setscore(0)
+       setcurrentDice(0)
+       setselected(0)
     }
   return (
     <div>
@@ -36,11 +51,19 @@ const Game = () => {
             </ul>
         </header>
         <p className={styles.error}>{selected==0 ?"Please select a number to play":""}</p>
+        <p className={styles.error}>There is a bug in this game the score updates on next click , i'll resolve it soon</p>
         <main className={styles.main}>
             <img src={images[currentDice]} onClick={generateRandom}/>
             <label htmlFor="img">Click on the dice to roll</label>
-            <button className={styles.resetBtn}>reset score</button>
-            <button className={styles.rules}>show rules</button>
+            <button className={styles.resetBtn} onClick={resetscore}>reset score</button>
+            <button className={styles.rules} onClick={()=>setrules(true)}>show rules</button>
+            {rules?<div className={styles.rulebox}>
+                <h5 onClick={()=>setrules(false)}>Hide Rules</h5>
+                <h4>Rules of this game</h4>
+                <span>select a number from top right</span>
+                <p>Click on the dice</p>
+                <p>if selected number matched the dice number you won else lose</p>
+            </div>:""}
         </main>
     </div>
   )
