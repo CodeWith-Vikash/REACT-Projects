6342213ae21b1f67e487c705db340035c2f6fd21
vikash <vp3637637@gmail.com>
2024-04-13 19:59:05 +0530

    add and search profile

--- firebase-contact/src/components/Form.jsx
+++ firebase-contact/src/components/Form.jsx
@@ -1,81 +1,82 @@
-
-import { Timestamp, doc, setDoc } from 'firebase/firestore'
-import React, { useState } from 'react'
-import {db} from '../firebase'
-import { NavLink } from 'react-router-dom'
+import { Timestamp, doc, setDoc } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { db, storage } from '../firebase';
+import { NavLink } from 'react-router-dom';
+import { BiSolidImageAdd } from "react-icons/bi";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Input = () => {
-  const [name, setname] = useState("")
-  const [email, setemail] = useState("")
-  const [phone, setphone] = useState("")
-  const [address, setaddress] = useState("")
-  const [message, setmessage] = useState("")
-  const handleSubmit=async (e)=>{
-    e.preventDefault()
-    const formdata={
-       name,
-       email,
-       phone,
-       address,
-       message,
-       date:Timestamp.fromDate(new Date())
-    }
-    await setDoc(doc(db,"data",name),formdata)
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const phone = e.target[2].value;
+    const address = e.target[3].value;
+    const message = e.target[4].value;
+    const imageFile = e.target[5].files[0];
+
+    const storageRef = ref(storage, name);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      null,
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          const formData = {
+            name,
+            email,
+            phone,
+            address,
+            message,
+            date: Timestamp.fromDate(new Date()),
+            imageurl: downloadURL
+          };
+          await setDoc(doc(db, "data", name), formData);
+          console.log(formData);
+        });
+      }
+    );
+  };
+
   return (
     <div className='bg-gray-600 min-h-screen flex justify-center items-center'> 
-       <form className='bg-white p-10 rounded-lg flex flex-col gap-8 m-6 max-w-[600px]' onSubmit={handleSubmit}>
-          <h1 className='font-semibold text-xl'>Contact</h1>
-           <section className='flex flex-wrap gap-5 '>
-           <div className='flex flex-col gap-1'>
-              <label htmlFor="" className='text-sm text-gray-300'>Your name</label>
-              <input type="text" placeholder='Enter your name' className='outline-none border-b-2 border-zinc-200 p-2'
-               value={name}
-               onChange={(e)=> setname(e.target.value)}
-              />
-            </div>
-
-
-            <div className='flex flex-col gap-1'>
-              <label htmlFor="" className='text-sm text-gray-300'>Email</label>
-              <input type="text" placeholder='Enter your Email' className='outline-none border-b-2 border-zinc-200 p-2'
-              value={email}
-               onChange={(e)=> setemail(e.target.value)}
-              />
-            </div>
-
-
-            <div className='flex flex-col gap-1'>
-              <label htmlFor="" className='text-sm text-gray-300'>Phone Number</label>
-              <input type="text" placeholder='Enter your contact no.' className='outline-none border-b-2 border-zinc-200 p-2'
-               value={phone}
-                onChange={(e)=> setphone(e.target.value)}
-              />
-            </div>
-
-
-            <div className='flex flex-col gap-1'>
-              <label htmlFor="" className='text-sm text-gray-300'>Address</label>
-              <input type="text" placeholder='Enter your address' className='outline-none border-b-2 border-zinc-200 p-2'
-               value={address}
-                onChange={(e)=> setaddress(e.target.value)}
-              />
-            </div>
-
-
-            <div className='flex flex-col gap-1'>
-              <label htmlFor="" className='text-sm text-gray-300'>Message</label>
-              <textarea cols="30" rows="5" placeholder='your message here..' className='outline-none border-b-2 border-zinc-200 p-2'
-               value={message}
-                onChange={(e)=> setmessage(e.target.value)}
-              ></textarea>
-            </div>
-           </section>
-           <button className='bg-red-600 text-white rounded-lg py-2'>Submit</button>
-       <p>You have to get data? <NavLink to="/search" className="text-red-600">Search</NavLink></p>
-       </form>
+      <form className='bg-white p-10 rounded-lg flex flex-col gap-8 m-6 max-w-[600px]' onSubmit={(e) => handleSubmit(e)}>
+        <h1 className='font-semibold text-xl'>Register Data</h1>
+        <section className='flex flex-wrap gap-5'>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="" className='text-sm text-gray-300'>Your name</label>
+            <input type="text" placeholder='Enter your name' className='outline-none border-b-2 border-zinc-200 p-2'/>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="" className='text-sm text-gray-300'>Email</label>
+            <input type="text" placeholder='Enter your Email' className='outline-none border-b-2 border-zinc-200 p-2'/>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="" className='text-sm text-gray-300'>Phone Number</label>
+            <input type="text" placeholder='Enter your contact no.' className='outline-none border-b-2 border-zinc-200 p-2'/>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="" className='text-sm text-gray-300'>Address</label>
+            <input type="text" placeholder='Enter your address' className='outline-none border-b-2 border-zinc-200 p-2'/>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="" className='text-sm text-gray-300'>Message</label>
+            <textarea cols="30" rows="5" placeholder='your message here..' className='outline-none border-b-2 border-zinc-200 p-2'></textarea>
+          </div>
+          <div>
+            <label htmlFor="image" className='flex items-center gap-2 cursor-pointer'><BiSolidImageAdd size="2rem" color='red'/><span>Add your profile pic</span></label>
+            <input type="file" id='image' className='hidden'/>
+          </div>
+        </section>
+        <NavLink to="/search"><button className='bg-red-600 text-white rounded-lg py-2 px-4'>Submit</button></NavLink>
+        <p>Do you want to search data ? <NavLink to="/search" className="text-red-600">Search</NavLink></p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
--- firebase-contact/src/components/Search.jsx
+++ firebase-contact/src/components/Search.jsx
@@ -1,55 +1,68 @@
-import { doc, getDoc } from 'firebase/firestore'
-import React, { useState } from 'react'
-import { db } from '../firebase'
-import { NavLink } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { db } from '../firebase';
+import { NavLink } from 'react-router-dom';
 
 const Search = () => {
-  const [inputval, setinputval] = useState("")
-  const [data, setdata] = useState({})
-  const [err, seterr] = useState(false)
-  const [isloading, setisloading] = useState(false)
-  const getdata=async()=>{
-    setisloading(true)
-      try {
-        const userdata= await getDoc(doc(db,"data",inputval))
-        if(userdata.exists()){
-          setdata(userdata.data())
-          setisloading(false)
-        }else{
-          seterr(true)
-          setisloading(false)
-        }
-      } catch (error) {
-          seterr(true)
-          setisloading(false)
+  const [inputVal, setInputVal] = useState("vikash");
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getData = async () => {
+    setIsLoading(true);
+    try {
+      const userData = await getDoc(doc(db, "data", inputVal));
+      if (userData.exists()) {
+        setData(userData.data());
+      } else {
+        setError(true);
       }
-      
-  }
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className='h-screen bg-gray-600 flex pt-10 gap-10 items-center flex-col'>
       <div className="flex gap-3">
-        <input type="text"  placeholder='Search user' className='px-4 rounded-lg outline-none' autoFocus
-          value={inputval}
-          onChange={(e)=> setinputval(e.target.value)}
+        <input
+          type="text"
+          placeholder='Search user'
+          className='px-4 rounded-lg outline-none'
+          autoFocus
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
         />
-        <button className='bg-red-600 text-white rounded-lg p-2' onClick={getdata}>Search</button>
+        <button className='bg-red-600 text-white rounded-lg p-2' onClick={getData}>Search</button>
       </div>
-      <section className='bg-white flex flex-col gap-3 p-6 rounded-lg min-h-[50vh] w-[300px] '>{
-        isloading?<b className='text-green-500 text-center'>Loading...</b>:err?<span className='text-red-600 font-semibold text-center'>data not found</span>:
-         <div>
-             <img src="https://up.yimg.com/ib/th?id=OIP.iptzZYdxQX_HJQSWlQBRFgHaHa&pid=Api&rs=1&c=1&qlt=95&w=103&h=103" className='h-[100px] w-[100px] my-0 mx-auto rounded-full'/>
-        <div className='flex flex-col gap-3'>
-        <p><b>Name</b> : {data.name}</p>
-        <p><b>Email</b> : {data.email}</p>
-        <p><b>Contact</b> : {data.phone}</p>
-        <p><b>Address</b> : {data.address}</p>
-        <p><b>Message</b>: {data.message}</p>
-        <p>Do you need to add date? <NavLink to="/" className="text-red-600">Form</NavLink></p>
-        </div>
-         </div>
-      }</section>
+      <section className='bg-white flex flex-col gap-3 p-6 rounded-lg min-h-[50vh] w-[300px] '>
+        {isLoading ? (
+          <b className='text-green-500 text-center'>Loading...</b>
+        ) : error ? (
+          <>
+          <span className='text-red-600 font-semibold text-center'>Data not found</span>
+          <br />
+          <p>Do you want to add data? <NavLink to="/" className="text-red-600">Form</NavLink></p>
+          </>
+        ) : (
+          <div>
+            <img src={data.imageurl} className='h-[100px] w-[100px] my-0 mx-auto rounded-full'/>
+            <div className='flex flex-col gap-3'>
+              <p><b>Name</b> : {data.name}</p>
+              <p><b>Email</b> : {data.email}</p>
+              <p><b>Contact</b> : {data.phone}</p>
+              <p><b>Address</b> : {data.address}</p>
+              <p><b>Message</b>: {data.message}</p>
+              <p>Do you want to add data? <NavLink to="/" className="text-red-600">Form</NavLink></p>
+            </div>
+          </div>
+        )}
+      </section>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
