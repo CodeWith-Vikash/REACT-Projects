0939c2c2c57c005366afcc3f6e68035595dfcb28
vikash <vp3637637@gmail.com>
2024-06-02 07:20:18 +0530

    added emoji picker

--- whatsapp/src/components/Home/ChatBox.jsx
+++ whatsapp/src/components/Home/ChatBox.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import { FaArrowAltCircleLeft,FaArrowAltCircleRight } from "react-icons/fa";
-import EmojiPicker from 'emoji-picker-react';
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
 import { MdEmojiEmotions } from "react-icons/md";
+import { BiSolidImageAdd } from "react-icons/bi";
 
 const ChatBox = ({openSide}) => {
   const [isEmojiVisible, setisEmojiVisible] = useState(false)
+  const [inputval, setinputval] = useState('')
+
   return (
     <div>
         <nav className='h-[9.2vh] bg-zinc-800 flex items-center px-2 gap-4'>
@@ -17,14 +21,23 @@ const ChatBox = ({openSide}) => {
          <section className=' h-[80.8vh] relative'>
           {/* emoji picker */}
           {isEmojiVisible && <div className='absolute bottom-1 left-1 z-[0]'>
-          <EmojiPicker/>
+          <Picker data={data} onEmojiSelect={(e)=> setinputval(prev => prev+e.native)} />
           </div>}
           {/* chat box */}
          </section>
          <section className='h-[10vh] bg-black flex items-center gap-4 px-2'>
                 <MdEmojiEmotions size="2rem" className='cursor-pointer' onClick={()=> setisEmojiVisible(!isEmojiVisible)}/>
+
+                <input type="file" id='image' className='hidden'/>
+                <label htmlFor="image">
+                  <BiSolidImageAdd size="2rem"/>
+                </label>
+
                 <div className='flex items-center gap-2'>
-                <input type="text" placeholder='Write something...' className='px-2 py-1 rounded w-[50vw]'/>
+                <input type="text" placeholder='Write something...' className='px-2 py-1 rounded w-[60vw] text-black outline-none'
+                value={inputval}
+                onChange={(e)=>setinputval(e.target.value)}
+                />
                 <button className='bg-green-600 py-1 px-2 font-semibold rounded'>send</button>
                 </div>
          </section>
