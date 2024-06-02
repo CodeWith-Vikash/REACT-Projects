1c826bc00d06c5b1bb1a2e9e5b8057109682197f
vikash <vp3637637@gmail.com>
2024-06-02 10:08:59 +0530

    chat ui completed

--- whatsapp/src/components/Home/ChatBox.jsx
+++ whatsapp/src/components/Home/ChatBox.jsx
@@ -18,12 +18,38 @@ const ChatBox = ({openSide}) => {
             <span>name</span>
           </div>
         </nav>
-         <section className=' h-[80.8vh] relative'>
+         <section className='relative h-[80.8vh] bg-gradient-to-b from-zinc-900 via-black to-zinc-900 p-4 overflow-auto flex flex-col gap-6'>
           {/* emoji picker */}
-          {isEmojiVisible && <div className='absolute bottom-1 left-1 z-[0]'>
+          {isEmojiVisible && <div className='absolute bottom-1 left-1 z-[0] flex-shrink-0'>
           <Picker data={data} onEmojiSelect={(e)=> setinputval(prev => prev+e.native)} />
           </div>}
           {/* chat box */}
+            <div className='flex gap-2 flex-row-reverse'> 
+            <img src="https://up.yimg.com/ib/th?id=OIP.7PFJ2-xr8x_3EprzHWmXAgHaHa&pid=Api&rs=1&c=1&qlt=95&w=119&h=119" alt="" className='h-10 rounded-full'/>
+             <p className='bg-green-500 w-fit p-2 mt-4 rounded-xl text-sm font-semibold rounded-tr-sm h-fit'>hello buddy</p>
+            </div>
+            {/* sender chat */}
+            <div className='flex gap-2 '> 
+            <img src="https://up.yimg.com/ib/th?id=OIP.Bv23QwiO7nTO6tvLBeGLLwHaHa&pid=Api&rs=1&c=1&qlt=95&w=120&h=120" alt="" className='h-10 rounded-full'/>
+             <p className='bg-blue-500 w-fit p-2 mt-4 rounded-xl text-sm font-semibold rounded-tl-sm h-fit'>what's up dude</p>
+            </div>
+            {/*  image with text*/}
+            <div className='flex gap-2 '> 
+            <img src="https://up.yimg.com/ib/th?id=OIP.Bv23QwiO7nTO6tvLBeGLLwHaHa&pid=Api&rs=1&c=1&qlt=95&w=120&h=120" alt="" className='h-10 rounded-full'/>
+             <div className='bg-blue-500 w-fit p-1 mt-4 rounded-xl text-sm font-semibold rounded-tl-sm h-fit'>
+              <img src="/wallpaper.jpg" alt=""  className='w-[250px] rounded-lg'/>
+              <p className='p-2'>this is a test image</p>
+             </div>
+            </div>
+            {/* for sender also */}
+            <div className='flex gap-2 flex-row-reverse'> 
+            <img src="https://up.yimg.com/ib/th?id=OIP.7PFJ2-xr8x_3EprzHWmXAgHaHa&pid=Api&rs=1&c=1&qlt=95&w=119&h=119" alt="" className='h-10 rounded-full'/>
+             <div className='bg-green-500 w-fit p-1 mt-4 rounded-xl text-sm font-semibold rounded-tr-sm h-fit'>
+              <img src="https://up.yimg.com/ib/th?id=OIP.a_NBaDZCwiV6fKa7XJ6B9AHaEK&pid=Api&rs=1&c=1&qlt=95&w=204&h=114" alt=""  className='w-[250px] rounded-lg'/>
+              <p className='p-2'>this is a test image</p>
+             </div>
+            </div>
+
          </section>
          <section className='h-[10vh] bg-black flex items-center gap-4 px-2'>
                 <MdEmojiEmotions size="2rem" className='cursor-pointer' onClick={()=> setisEmojiVisible(!isEmojiVisible)}/>
